520914213e61c3e85597917ee9fafdc831658470
Thomas <Thomas>
2021-10-28 17:59:28 +0200

    add code Donst

---
+++ Unity/KI/Assets/Scripts/GA/Terminators/TerminatorDonst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminatorDonst : ITerminator
+{
+    public bool JudgementDay(GenerationDB.Generation generation)
+    {
+        return generation.Fittest.Fitness >= 0.9f;
+    }
+}
